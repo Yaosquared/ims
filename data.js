b9cdc7a39a83f3ba75b4cf6e0e85aedc0e924d06
Yaosquared <marioyaoyao14@gmail.com>
2024-12-02 18:08:08 +0800

    feat: initial commit

---
+++ data.js
@@ -0,0 +1,28 @@
+const placeholderData = [
+  {
+    id: 1,
+    name: "Sample Item 1",
+    category: "Electronics",
+    quantity: 10,
+    price: "150.00", // Parse string to float/decimal
+    description: "A sample electronic item 1",
+  },
+  {
+    id: 2,
+    name: "Sample Item 2",
+    category: "Electronics",
+    quantity: 15,
+    price: "150.123", // Parse string to float/decimal
+    description: "A sample electronic item 2",
+  },
+  {
+    id: 2,
+    name: "Sample Item 3",
+    category: "Electronics",
+    quantity: 15,
+    price: "150.456", // Parse string to float/decimal
+    description: "A sample electronic item 3",
+  },
+];
+
+module.exports = placeholderData;
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+const placeholderData = require("./data");
+
+const app = express();
+// get styles from public/assets folder
+app.use("/styles", express.static("styles"));
+const mappedData = placeholderData.map((item) => {
+  return {
+    name: item.name,
+    category: item.category,
+    quantity: item.quantity,
+    price: parseFloat(item.price),
+    description: item.description,
+  };
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  res.render("homePage", { mappedData });
+});
+
+app.get("/edit", (req, res) => {
+  res.render("editItem", { mappedData });
+});
+
+app.get("/add", (req, res) => {
+  res.render("addItem", { mappedData });
+});
+
+app.get("/delete", (req, res) => {
+  res.render("deleteItem", { mappedData });
+});
+
+app.get("*", (req, res) => {
+  res.send("Page not available");
+});
+
+// app.get("/", (req, res) => {
+//   res.send(mappedData);
+// });
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000...");
+});
