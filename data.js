43fc3284d5b4b00757fe11cd39b9845c56bbd5ee
Yaosquared <marioyaoyao14@gmail.com>
2024-12-07 19:40:19 +0800

    feat: added crud functionality with expres.js

--- data.js
+++ data.js
@@ -1,26 +1,28 @@
-const placeholderData = [
+const { v4: uuidv4 } = require("uuid");
+
+let placeholderData = [
   {
-    id: 1,
+    id: uuidv4(),
     name: "Sample Item 1",
     category: "Electronics",
     quantity: 10,
-    price: "150.00", // Parse string to float/decimal
+    price: "150.00",
     description: "A sample electronic item 1",
   },
   {
-    id: 2,
+    id: uuidv4(),
     name: "Sample Item 2",
     category: "Electronics",
     quantity: 15,
-    price: "150.123", // Parse string to float/decimal
+    price: "150.13",
     description: "A sample electronic item 2",
   },
   {
-    id: 2,
+    id: uuidv4(),
     name: "Sample Item 3",
     category: "Electronics",
     quantity: 15,
-    price: "150.456", // Parse string to float/decimal
+    price: "150.45",
     description: "A sample electronic item 3",
   },
 ];
--- index.js
+++ index.js
@@ -1,43 +1,92 @@
 const express = require("express");
 const path = require("path");
-const placeholderData = require("./data");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
 const app = express();
-// get styles from public/assets folder
+const placeholderData = require("./data");
+
+// Get static files from local directories
 app.use("/styles", express.static("styles"));
 app.use("/assets", express.static("assets"));
 
-const mappedData = placeholderData.map((item) => {
-  return {
-    name: item.name,
-    category: item.category,
-    quantity: item.quantity,
-    price: parseFloat(item.price),
-    description: item.description,
-  };
-});
+// Parse any data from form
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.get("/", (req, res) => {
-  res.render("homePage", { mappedData });
+// Display form to add new item
+app.get("/inventory/new", (req, res) => {
+  res.render("inventory/new");
 });
 
-app.get("/edit", (req, res) => {
-  res.render("editItem", { mappedData });
+// Create new item
+app.post("/inventory", (req, res) => {
+  const { name, category, quantity, price, description } = req.body;
+  placeholderData.push({
+    id: uuidv4(),
+    name,
+    category,
+    quantity,
+    price,
+    description,
+  });
+  res.redirect("/inventory");
 });
 
-app.get("/add", (req, res) => {
-  res.render("addItem", { mappedData });
+// Display specific details of item
+app.get("/inventory/:id", (req, res) => {
+  const { id } = req.params;
+  const itemDetails = placeholderData.find((item) => item.id === id);
+  res.render("inventory/details", { itemDetails });
 });
 
-app.get("/delete", (req, res) => {
-  res.render("deleteItem", { mappedData });
+// Display edit form with exisintg data mapped to it
+app.get("/inventory/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const itemDetails = placeholderData.find((item) => item.id === id);
+  res.render("inventory/edit", { itemDetails });
 });
 
-app.get("*", (req, res) => {
-  res.send("Page not available");
+// Display form to update item
+app.patch("/inventory/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, category, quantity, price, description } = req.body;
+  const itemIndex = placeholderData.findIndex((item) => item.id === id);
+  placeholderData[itemIndex] = {
+    id,
+    name,
+    category,
+    quantity,
+    price,
+    description,
+  };
+  res.redirect("/inventory");
+});
+
+// Display all data in tabular format
+app.get("/inventory", (req, res) => {
+  res.render("inventory/home", { placeholderData });
+});
+
+// Display confirmation for delete modal
+app.get("/inventory/:id/delete", (req, res) => {
+  const { id } = req.params;
+  itemToDelete = placeholderData.find((item) => item.id === id);
+  // console.log(itemToDelete);
+  res.render("inventory/delete", { itemToDelete });
+});
+
+// Delete an item
+app.delete("/inventory/:id", (req, res) => {
+  const { id } = req.params;
+  itemIndex = placeholderData.findIndex((item) => item.id === id);
+  // console.log(itemIndex);
+  placeholderData.splice(itemIndex, 1);
+  res.redirect("/inventory");
 });
 
 app.listen(3000, () => {
